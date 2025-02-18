aa395bca9d144e02630e851d5a73d4cb84ae21d3
Yash Kalange <yashkalange127@gmail.com>
2025-02-18 14:26:04 +0530

    starting editor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 // import MyCard from './components/MyCard'
 import { ThemeProvider } from "@/components/theme-provider"
 import { useState } from 'react';
+import Editor from '@monaco-editor/react'
 
 import {
   Card,
@@ -12,11 +13,29 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import {
+  Sidebar,
+  SidebarContent,
+  // SidebarFooter,
+  SidebarGroup,
+  // SidebarHeader,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+} from "@/components/ui/sidebar"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarProvider } from "@/components/ui/sidebar"
+
 
 
 
@@ -34,6 +53,7 @@ function App() {
     }
   }
   
+
 
   if(!joined)
   {
@@ -81,13 +101,74 @@ function App() {
 </div>
       )
   }
-  
+  const copyRoomId = () =>{
+     
+  }
+  const leaveRoom = () =>{
+
+  }
   return(  
     
-      <SidebarProvider>
-      <AppSidebar />
-    </SidebarProvider>  
-  
+      <div className='bg-slate-950 w-screen'>
+        
+        <div>
+        <SidebarProvider>
+      <Sidebar className="w-[350px]">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>DevsCanvas</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+            <div className='mt-8 flex flex-col '>
+              <p className='px-4'>Room Code : {roomId} </p> 
+              <Button className='mx-16 m-4' onClick={copyRoomId} >
+                Copy Id
+              </Button>
+            </div>
+            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+            <div className='p-2'>
+              <p className='text-xl pb-4'>Users in Room</p>
+              <ul className='text-lg p-4'>
+                <li>YASH</li>
+                <li>Aditya</li>
+                <li>KAMNA</li>
+              </ul>
+            </div>
+            <div>
+              <p>
+                User typing...
+              </p>
+            </div>
+            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+            <div className='px-20'>
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Language" />
+              </SelectTrigger>
+              <SelectContent >
+                <SelectItem value="javascript">JavaScript</SelectItem>
+                <SelectItem value="python">Python</SelectItem>
+                <SelectItem value="java">Java</SelectItem>
+                <SelectItem value="cpp">C++</SelectItem>
+              </SelectContent>
+            </Select>
+            </div>
+            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+            <Button className='mx-16 mt-4' onClick={leaveRoom} >
+                Leave Room
+              </Button>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent> 
+    </Sidebar>
+    </SidebarProvider>
+      </div> 
+      <div className='bg-violet-950'>
+
+        dghj
+        </div>  
+      </div>
 )
 }
 
